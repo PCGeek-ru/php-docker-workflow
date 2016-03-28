cc8cdca520ca3d13870bdde6b093995e5101a691
PCGeek <volkov.slava31@gmail.com>
2016-03-28 21:53:02 +0500

    add default infopage

---
+++ domains/sites/html/www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	    <title>Info page</title>
+	    <!--<link rel="stylesheet" href="css/main.css">-->
+	</head>
+	<body>
+		<h1>Available PHP versions</h1>
+		<a href="http://php-5-4.dev">PHP 5.4 info</a><br/>
+		<a href="http://php-7.dev">PHP 7 info</a><br/>
+		<h2>Utils</h2>
+		<a href="/phpmyadmin">PhpMyAdmin</a><br/>	
+	</body>	
